dbd73283d248289f4f928c89185ca89a7802f30d
ClydeCarey <73868789+ClydeCarey@users.noreply.github.com>
2020-11-25 12:54:48 -1000

    Initial commit

---
+++ Assets/GameDevHQ/Challenge/Challenge_Easy_Spawn_Minion/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _minionPrefab;
+    [SerializeField]
+    private GameObject _minionStartPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _minionStartPosition = GameObject.Find("Spawn_Position");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_minionPrefab, _minionStartPosition.transform.position, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/GameDevHQ/Challenge/Challenge_Easy_Spawn_Minion/Scripts/MinionBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * 2.0f * Time.deltaTime);
+        if (this.transform.position.x > 2.0f)
+        {
+            this.GetComponent<Renderer>().material.SetColor("_Color", Color.green) ;
+        }
+        if (this.transform.position.x > 5.5f)
+        {
+            Destroy(this, 2.0f);
+        }
+        
+    }
+}
